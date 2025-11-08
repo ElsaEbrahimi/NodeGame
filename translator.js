71b0a581faa32796826f39f8446b5d716b210432
Elsa Ebrahimi <teaserpluss@gmail.com>
2025-11-08 20:58:20 +0330

    translator

--- translator.js
+++ translator.js
@@ -4,8 +4,13 @@ if (userInput.length === 0) {
   console.error("Please write a Word");
   process.exit();
 }
+
 let firstChar = userInput[0][0];
+let secondChar = userInput[0][1];
+
+// console.log(`${secondChar}`);
 // console.log(`${firstChar}`);
+
 const vowels = ["a", "e", "i", "o", "u"];
 // prettier-ignore
 const consonants = [
@@ -13,14 +18,23 @@ const consonants = [
   'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
 ];
 
-
-
-const checkWord = function (userInput,firstChar, vowel, consonants) {
+const checkWord = function () {
   console.log(`you write : ${userInput}`);
 
   if (vowels.includes(firstChar)) {
-let input=
+    console.log(`Pig latin Translated:${userInput}way`);
+  } else if (
+    consonants.includes(firstChar) &&
+    consonants.includes(secondChar)
+  ) {
+    let word = userInput.join("").slice(2);
+    console.log(`Pig latin Translated:${word}${firstChar}${secondChar}ay`);
+  } else if (consonants.includes(firstChar) && vowels.includes(secondChar)) {
+    let word = userInput.join("").slice(1);
+    console.log(`Pig latin Translated:${word}${firstChar}ay`);
+  } else {
+    console.log(`please write word only with letters`);
   }
-  else if
-
+  return;
 };
+checkWord(userInput);
